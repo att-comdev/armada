501d3d5ca2bbb54d714ca67bfae8e079a935ae92
Alan Meadows <alan.meadows@gmail.com>
2017-02-10 15:27:04 -0800

    Initial Armada Commit (#1)
    * Initial commit of Armada framework
    
    * python based .gitignore
    
    * incorporate pull request feedback

---
+++ armada/logutil.py
@@ -0,0 +1,17 @@
+# various utilties to support logging
+
+import logging
+from logging.handlers import SysLogHandler
+LOG = logging.getLogger('armada')
+LOG_FORMAT='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+LOG_DATE = '%m-%d %H:%M'
+
+
+
+def setup_logging(args):
+    level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(level=level, format=LOG_FORMAT, date_fmt=LOG_DATE)
+    handler = SysLogHandler(address='/dev/log')
+    syslog_formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
+    handler.setFormatter(syslog_formatter)
+    LOG.addHandler(handler)
