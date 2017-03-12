cb718b6604afbe19a8a288e314a3a6c7cf538451
gardlt <gardlt@tetrasol.org>
2017-03-11 20:13:56 -0600

    [script] installing-libgit2-deps
    * removed pip from requirements.txt
    * added bash for libgit install
    * updated tox

---
+++ scripts/libgit2.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Ubuntu 16.04 Install only
+
+sudo apt install git cmake make -y
+sudo apt-get install -y python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libssh2-1 libgit2-dev python-pip libgit2-24
+sudo apt-get install -y pkg-config libssh2-1-dev libhttp-parser-dev libssl-dev libz-dev
+
+LIBGIT_VERSION='0.25.0'
+
+wget https://github.com/libgit2/libgit2/archive/v${LIBGIT_VERSION}.tar.gz
+tar xzf v${LIBGIT_VERSION}.tar.gz
+cd libgit2-${LIBGIT_VERSION}/
+cmake .
+make
+sudo make install
+sudo pip install pygit2==0.25.0
+sudo ldconfig
