8cf9009313183a05f4f24eed4f3b4ae8ff2663c3
drewwalters96 <drewwalters96@gmail.com>
2017-06-26 10:39:52 -0500

    [Feat] Add API support for core features
    - Refactor API structure
    - Add API support for existing CLI flags
    - Add Keystone token and RBAC authentication
    - Add API documentation
    - Add API unit tests

---
+++ armada/api/server.py
@@ -0,0 +1,57 @@
+# Copyright 2017 The Armada Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from armada.conf import default
+
+# Required Oslo configuration setup
+default.register_opts()
+
+from armada_controller import Apply
+from tiller_controller import Release, Status
+
+from middleware import AuthMiddleware, RoleMiddleware
+
+import falcon
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+DOMAIN = "armada"
+
+logging.setup(CONF, DOMAIN)
+
+# Build API
+def create(middleware=CONF.middleware):
+    if middleware:
+        api = falcon.API(middleware=[AuthMiddleware(), RoleMiddleware()])
+    else:
+        api = falcon.API()
+
+    # Configure API routing
+    url_routes = (
+        ('/tiller/status', Status()),
+        ('/tiller/releases', Release()),
+        ('/armada/apply/', Apply())
+    )
+
+    for route, service in url_routes:
+        api.add_route(route, service)
+
+    return api
+
+
+api = create()
---
+++ armada/tests/unit/api/test_api.py
@@ -0,0 +1,76 @@
+# Copyright 2017 The Armada Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from armada.api import server
+from falcon import testing
+
+import json
+import mock
+
+class APITestCase(testing.TestCase):
+    def setUp(self):
+        super(APITestCase, self).setUp()
+
+        self.app = server.create(middleware=False)
+
+class TestAPI(APITestCase):
+    @mock.patch('armada.api.armada_controller.Handler')
+    def test_armada_apply(self, mock_armada):
+        '''
+        Test /armada/apply endpoint
+        '''
+        mock_armada.sync.return_value = None
+
+        body = json.dumps({'file': '../examples/openstack-helm.yaml',
+                           'options': {'debug': 'true',
+                                       'disable_update_pre': 'false',
+                                       'disable_update_post': 'false',
+                                       'enable_chart_cleanup': 'false',
+                                       'skip_pre_flight': 'false',
+                                       'dry_run': 'false',
+                                       'wait': 'false',
+                                       'timeout': '100'}})
+
+        doc = {u'message': u'Success'}
+
+        result = self.simulate_post(path='/armada/apply', body=body)
+        self.assertEqual(result.json, doc)
+
+    @mock.patch('armada.api.tiller_controller.tillerHandler')
+    def test_tiller_status(self, mock_tiller):
+        '''
+        Test /tiller/status endpoint
+        '''
+
+        # Mock tiller status value
+        mock_tiller.tiller_status.return_value = 'Active'
+
+        doc = {u'message': u'Tiller Server is Active'}
+
+        result = self.simulate_get('/tiller/status')
+        self.assertEqual(result.json, doc)
+
+    @mock.patch('armada.api.tiller_controller.tillerHandler')
+    def test_tiller_releases(self, mock_tiller):
+        '''
+        Test /tiller/releases endpoint
+        '''
+
+        # Mock tiller status value
+        mock_tiller.list_releases.return_value = None
+
+        doc = {u'releases': {}}
+
+        result = self.simulate_get('/tiller/releases')
+        self.assertEqual(result.json, doc)
