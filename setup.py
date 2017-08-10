9e97f1f7b2c1c01627dc28761d8e0005128db635
Alexis Rivera De La Torre <gardlt@tetrasol.org>
2017-08-10 01:22:21 +0000

    [feat] adding-tox-testing
    - tox commands
    - docs update
    
    Change-Id: Ie6caa498d6017822e095ee5b001124074e2755aa

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-from setuptools.command.test import test as TestCommand
-import sys
 import setuptools
-
-class Tox(TestCommand):
-    """Runs Tox comands"""
-    def finalize_options(self):
-        """preps test suite"""
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        """runs test suite"""
-        import tox
-        errcode = tox.cmdline(self.test_args)
-        sys.exit(errcode)
-
 
 try:
     import multiprocessing  # noqa
