d17485f8b976df0ebe7cecff0ab542f23227ba1e
Alexis Rivera De La Torre <gardlt@tetrasol.org>
2017-08-10 01:22:21 +0000

    [feat] adding-tox-testing
    - tox commands
    - docs update
    
    Change-Id: Ie6caa498d6017822e095ee5b001124074e2755aa

--- armada/utils/source.py
+++ armada/utils/source.py
@@ -1,10 +1,25 @@
+# Copyright 2017 The Armada Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os import path
 import os
 import requests
+import shutil
 import tarfile
 import tempfile
-import shutil
+
 from git import Repo
-from os import path
 
 from ..exceptions import source_exceptions
 
@@ -20,7 +35,7 @@ def git_clone(repo_url, branch='master'):
         raise source_exceptions.GitLocationException(repo_url)
 
     os.environ['GIT_TERMINAL_PROMPT'] = '0'
-    _tmp_dir = tempfile.mkdtemp(prefix='armada', dir='/tmp')
+    _tmp_dir = tempfile.mkdtemp(prefix='armada')
 
     try:
         Repo.clone_from(repo_url, _tmp_dir, **{'branch': branch})
@@ -38,7 +53,7 @@ def download_tarball(tarball_url):
     Downloads a tarball to /tmp and returns the path
     '''
     try:
-        tarball_filename = tempfile.mkstemp(prefix='armada', dir='/tmp')[1]
+        tarball_filename = tempfile.mkstemp(prefix='armada')[1]
         response = requests.get(tarball_url)
         with open(tarball_filename, 'wb') as f:
             f.write(response.content)
@@ -53,7 +68,7 @@ def extract_tarball(tarball_path):
     if not path.exists(tarball_path):
         raise source_exceptions.InvalidPathException(tarball_path)
 
-    _tmp_dir = tempfile.mkdtemp(prefix='armada', dir='/tmp')
+    _tmp_dir = tempfile.mkdtemp(prefix='armada')
 
     try:
         file = tarfile.open(tarball_path)
