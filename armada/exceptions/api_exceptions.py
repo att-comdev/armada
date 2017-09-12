4cc269a04bc92c10a47cd76e6916a42a09e31962
gardlt <gardlt@tetrasol.org>
2017-09-12 03:11:57 +0000

    feat(api): policy enforcement and api standard
    - enhanced logging
    - created base structure
    - updated docs
    - PasteDeploy auth
    - Oslo Policy
    
    Closes #107
    
    Change-Id: I805863c57f17fcfb26dac5d03efb165e4be49a4e

---
+++ armada/exceptions/api_exceptions.py
@@ -0,0 +1,36 @@
+# Copyright 2017 The Armada Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import base_exception as base
+
+class ApiException(base.ArmadaBaseException):
+    '''Base class for API exceptions and error handling.'''
+
+    message = 'An unknown API error occur.'
+
+
+class ApiBaseException(ApiException):
+    '''Exception that occurs during chart cleanup.'''
+
+    def __init__(self):
+        self._message = 'There was an error listing the helm chart releases.'
+        super(ApiBaseException, self).__init__(self._message)
+
+
+class ApiJsonException(ApiException):
+    '''Exception that occurs during chart cleanup.'''
+
+    def __init__(self):
+        self._message = 'There was an error listing the helm chart releases.'
+        super(ApiJsonException, self).__init__(self._message)
