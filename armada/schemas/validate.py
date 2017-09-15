f0e6d89d87800512b28a3dad77cf35693672ac11
gardlt <gardlt@tetrasol.org>
2017-09-15 03:45:06 +0000

    [WIP] feat(validate): using jsonschema for validation
    - created armada schema artifact
    
    Change-Id: Idb91fa552d3d7a3d7d525609d505fe7380443238

---
+++ armada/schemas/validate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import yaml
+from jsonschema import validate
+chart = 'armada/Chart/v1'
+chart_group = 'armada/ChartGroup/v1'
+manifest = 'armada/Manifest/v1'
+
+SCHEMA = {}
+
+def _load_schemas():
+
+def validate_armada_documents(schema, documents):
+    pass
+
+
+
+
+
+
+
+schema = yaml.safe_load(open('armada-manifest-schema.yaml').read())
+print schema
+
+manifest = yaml.safe_load(open('simple-test.yaml').read())
+print manifest
+
+print validate(manifest.get('data'), schema.get('data'))
