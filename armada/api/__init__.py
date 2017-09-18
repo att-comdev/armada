c5b7371f2f713b3673abfe70c7b66079c0205ac4
gardlt <gardlt@tetrasol.org>
2017-09-18 00:24:10 +0000

    style(armada): converting py2 to py3
    - Format code to python 3.5
    
    Change-Id: I7414b5de915429c2c7f85b99f2ab91f395c62121

--- armada/api/__init__.py
+++ armada/api/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from builtins import str
+from builtins import object
 import json
 import uuid
 import logging as log
--- armada/exceptions/armada_exceptions.py
+++ armada/exceptions/armada_exceptions.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import base_exception
+from . import base_exception
 
 
 class ArmadaException(base_exception.ArmadaBaseException):
