80aabda4bd317575af494f237e3ecfa6e72f6c40
gardlt <gardlt@tetrasol.org>
2017-09-19 05:42:45 +0000

    feat(cli): using-click-framework
    - using click framework
    - added api client
    - allow interactions between code and service endpoints
    - documention on the command line
    - updated gitignore
    
    Change-Id: Ibe359025f5b35606d876c29fa88e04048f276cc8

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,7 @@ PORT="8000"
 set -e
 
 if [ "$1" = 'server' ]; then
-    exec uwsgi --http 0.0.0.0:${PORT} --paste config:$(pwd)/etc/armada/api-paste.ini --enable-threads -L --pyargv " --config-file $(pwd)/etc/armada/armada.conf"
-fi
-
-if [ "$1" = 'tiller' ] || [ "$1" = 'apply' ]; then
+    exec uwsgi --http :${PORT} --http-timeout 3600 --paste config:/etc/armada/api-paste.ini --enable-threads -L --pyargv "--config-file /etc/armada/armada.conf"
+else
     exec $CMD "$@"
 fi
-
-exec "$@"
