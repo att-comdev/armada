e13147be6ffead5eafbcb2789dd3069739b9520c
Mark Burnett <mark.m.burnett@gmail.com>
2017-10-13 23:28:03 -0500

    fix(git): allow specifying git refs for sources
    This allows, e.g., pointing at a particular Gerrit Patch Set using
    natural configuration, e.g.:
    
        ---
        schema: armada/Chart/v1
        metadata:
          schema: metadata/Document/v1
          name: ceph-config
        data:
          chart_name: ceph-config
          release: ceph-config
          namespace: ucp
          timeout: 3600
          install:
            no_hooks: true
          upgrade:
            no_hooks: true
          values: {}
          source:
            type: git
            location: https://github.com/openstack/openstack-helm
            subpath: ceph
            reference: refs/changes/54/457754/73
          dependencies: []
    
    Change-Id: Ib6af0fec2bbfa1fa4e523d839df44af047697522

--- armada/tests/unit/utils/test_source.py
+++ armada/tests/unit/utils/test_source.py
@@ -12,40 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import shutil
+
 import mock
 import unittest
 
 from armada.exceptions import source_exceptions
-
 from armada.utils import source
 
 
 class GitTestCase(unittest.TestCase):
 
-    SOURCE_UTILS_LOCATION = 'armada.utils.source'
+    def _validate_git_clone(self, repo_dir, expected_ref=None):
+        self.assertTrue(os.path.isdir(repo_dir))
+        self.addCleanup(shutil.rmtree, repo_dir)
+        self.assertIn('armada', repo_dir)
+        # Assert that the repo is a Git repo.
+        self.assertTrue(os.path.isdir(os.path.join(repo_dir, '.git')))
+        if expected_ref:
+            # Assert the FETCH_HEAD is at the expected ref.
+            with open(os.path.join(repo_dir, '.git', 'FETCH_HEAD'), 'r') \
+                    as git_file:
+                self.assertIn(expected_ref, git_file.read())
 
-    @mock.patch('armada.utils.source.Git')
-    @mock.patch('armada.utils.source.tempfile')
-    @mock.patch('armada.utils.source.Repo')
-    def test_git_clone_good_url(self, mock_git_repo, mock_temp, mock_git_lib):
-        mock_temp.mkdtemp.return_value = '/tmp/armada'
-        mock_git_lib.checkout.return_value = "Repository"
+    def test_git_clone_good_url(self):
         url = 'http://github.com/att-comdev/armada'
+        git_dir = source.git_clone(url)
+        self._validate_git_clone(git_dir)
 
-        dir = source.git_clone(url)
-
-        self.assertIsNotNone(dir)
+    def test_git_clone_ref(self):
+        ref = 'refs/changes/54/457754/73'
+        git_dir = source.git_clone(
+            'https://github.com/openstack/openstack-helm', ref)
+        self._validate_git_clone(git_dir, ref)
 
     def test_git_clone_empty_url(self):
         url = ''
+        error_re = '%s is not a valid git repository.' % url
 
-        with self.assertRaises(Exception):
-            self.assertFalse(source.git_clone(url))
+        with self.assertRaisesRegexp(
+                source_exceptions.GitLocationException, error_re):
+            source.git_clone(url)
 
     def test_git_clone_bad_url(self):
         url = 'http://github.com/dummy/armada'
+        error_re = '%s is not a valid git repository.' % url
 
-        with self.assertRaises(Exception):
+        with self.assertRaisesRegexp(
+                source_exceptions.GitLocationException, error_re):
             source.git_clone(url)
 
     @mock.patch('armada.utils.source.tempfile')
@@ -58,8 +73,7 @@ class GitTestCase(unittest.TestCase):
         mock_requests.get.return_value = mock_response
 
         mock_open = mock.mock_open()
-        with mock.patch('{}.open'.format(self.SOURCE_UTILS_LOCATION),
-                        mock_open, create=True):
+        with mock.patch.object(source, 'open', mock_open, create=True):
             source.download_tarball(url)
 
         mock_temp.mkstemp.assert_called_once()
@@ -90,7 +104,7 @@ class GitTestCase(unittest.TestCase):
     def test_tarball_extract_bad_path(self, mock_tarfile, mock_path):
         mock_path.exists.return_value = False
         path = '/tmp/armada'
-        with self.assertRaises(Exception):
+        with self.assertRaises(source_exceptions.InvalidPathException):
             source.extract_tarball(path)
 
         mock_tarfile.open.assert_not_called()
@@ -115,6 +129,6 @@ class GitTestCase(unittest.TestCase):
     def test_source_cleanup_bad_path(self, mock_path, mock_shutil):
         mock_path.exists.return_value = False
         path = 'armada'
-        with self.assertRaises(Exception):
+        with self.assertRaises(source_exceptions.InvalidPathException):
             source.source_cleanup(path)
         mock_shutil.rmtree.assert_not_called()
