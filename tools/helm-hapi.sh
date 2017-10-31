5d866e96777dfee1e3d7f41bae11efc113fea33e
Alexis Rivera DeLa Torre <garivera89@gmail.com>
2017-10-30 23:10:24 -0500

    [WIP] feat(armada): adding makefile
    - adding makefile
    
    Change-Id: I064d2c95ebaa9fb16656ce36c9ea36c618f9bfad

--- tools/helm-hapi.sh
+++ tools/helm-hapi.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/kubernetes/helm ./helm -b $1
+HELM_BRANCH=$1
+
+if [ $HELM_BRANCH=='' ]; then
+    HELM_BRANCH = 'release-2.6.0'
+fi
+
+git clone https://github.com/kubernetes/helm ./helm -b $HELM_BRANCH
 
 python -m grpc_tools.protoc -I helm/_proto --python_out=. --grpc_python_out=. helm/_proto/hapi/chart/*
 python -m grpc_tools.protoc -I helm/_proto --python_out=. --grpc_python_out=. helm/_proto/hapi/services/*
