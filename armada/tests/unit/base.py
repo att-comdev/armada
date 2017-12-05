ab051529eda4b41252462a92af708fde14d2ef18
Felipe Monteiro <felipe.monteiro@att.com>
2017-12-05 21:16:11 +0000

    bug(tests): Unskip Armada API tests
    This PS sets the foundation for unskipping Armada API unit tests
    by making necessary framework changes where necessary as well
    as test refactoring so that the tests not only can be unskipped
    but also execute successfully.
    
    However, until a feature-rich testing framework is in place, it's
    not possible to do end-to-end testing as mocking is currently used
    to stub out Tiller API.
    
    Negative RBAC tests will be added in a follow-up.
    
    Included in this PS:
      - policy fixture for asserting that expected policies are enforced
      - unit tests for tiller/armada API
      - test_utils module
    
    Change-Id: I2f454b27b014875bc35fd706f7c0d05364ce562a

---
+++ armada/tests/unit/base.py
@@ -0,0 +1,70 @@
+# Copyright 2013 IBM Corp.
+# Copyright 2017 AT&T Intellectual Property.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import fixtures
+import mock
+from oslo_config import cfg
+import testtools
+
+CONF = cfg.CONF
+
+
+class ArmadaTestCase(testtools.TestCase):
+
+    def setUp(self):
+        super(ArmadaTestCase, self).setUp()
+        self.useFixture(fixtures.FakeLogger('armada'))
+
+    def override_config(self, name, override, group=None):
+        CONF.set_override(name, override, group)
+        self.addCleanup(CONF.clear_override, name, group)
+
+    def assertEmpty(self, collection):
+        if isinstance(collection, list):
+            self.assertEqual(0, len(collection))
+        elif isinstance(collection, dict):
+            self.assertEqual(0, len(collection.keys()))
+
+    def patch(self, target, autospec=True, **kwargs):
+        """Returns a started `mock.patch` object for the supplied target.
+
+        The caller may then call the returned patcher to create a mock object.
+
+        The caller does not need to call stop() on the returned
+        patcher object, as this method automatically adds a cleanup
+        to the test class to stop the patcher.
+
+        :param target: String module.class or module.object expression to patch
+        :param **kwargs: Passed as-is to `mock.patch`. See mock documentation
+                         for details.
+        """
+        p = mock.patch(target, autospec=autospec, **kwargs)
+        m = p.start()
+        self.addCleanup(p.stop)
+        return m
+
+    def patchobject(self, target, attribute, new=mock.DEFAULT, autospec=True):
+        """Convenient wrapper around `mock.patch.object`
+
+        Returns a started mock that will be automatically stopped after the
+        test ran.
+        """
+
+        p = mock.patch.object(target, attribute, new, autospec=autospec)
+        m = p.start()
+        self.addCleanup(p.stop)
+        return m
