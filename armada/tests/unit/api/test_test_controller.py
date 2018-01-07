3a1b42d7a5b6ee757883ab4011ed2a1c7d6270e2
Felipe Monteiro <felipe.monteiro@att.com>
2018-01-07 19:47:30 +0000

    Add RBAC tests to Armada to sanity-check RBAC implementation
    This PS adds negative RBAC API tests to Armada to sanity-check
    its RBAC implementation. One of the use cases for Armada even
    having an API server is RBAC. To that end, we should validate
    that it is working as intended in the negative case.
    
    Change-Id: If00f4ba45f694aa2ac556e7f4a940010a6d0a8a8

---
+++ armada/tests/unit/api/test_test_controller.py
@@ -0,0 +1,39 @@
+# Copyright 2017 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from armada.tests import test_utils
+from armada.tests.unit.api import base
+
+
+class TestControllerNegativeRbacTest(base.BaseControllerTest):
+
+    @test_utils.attr(type=['negative'])
+    def test_test_release_insufficient_permissions(self):
+        """Tests the GET /api/v1.0/test/{release} endpoint returns 403
+        following failed authorization.
+        """
+        rules = {'armada:test_release': 'rule:admin_api'}
+        self.policy.set_rules(rules)
+        resp = self.app.simulate_get('/api/v1.0/test/test-release')
+        self.assertEqual(403, resp.status_code)
+
+    @test_utils.attr(type=['negative'])
+    def test_tests_manifest_insufficient_permissions(self):
+        """Tests the POST /api/v1.0/tests endpoint returns 403 following failed
+        authorization.
+        """
+        rules = {'armada:tests_manifest': 'rule:admin_api'}
+        self.policy.set_rules(rules)
+        resp = self.app.simulate_post('/api/v1.0/tests')
+        self.assertEqual(403, resp.status_code)
