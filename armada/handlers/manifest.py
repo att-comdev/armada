577cd91e73cdbe76b9ddabc39b1926cf7b436de5
Felipe Monteiro <felipe.monteiro@att.com>
2018-01-22 21:44:30 +0000

    bug(manifest): Allow specific manifest to be specified
    This PS allows users to specify the manifest file to use
    by the Armada handler by introducing a new flag called
    `desired_manifest`. This flag was added to the API and
    CLI.
    
    Unit tests were added which build off those introduced
    by: https://review.gerrithub.io/#/c/395807/
    
    Also updates documentation to convey information about
    the new flag.
    
    Change-Id: I1d5a3ecc1e99b6479438d0ee5490610178be34fe

--- armada/handlers/manifest.py
+++ armada/handlers/manifest.py
@@ -12,40 +12,92 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from armada.const import DOCUMENT_CHART, DOCUMENT_GROUP, DOCUMENT_MANIFEST
+from oslo_log import log as logging
+
+from armada import const
+from armada import exceptions
+
+LOG = logging.getLogger(__name__)
 
 
 class Manifest(object):
-    def __init__(self, documents):
+
+    def __init__(self, documents, desired_manifest=None):
+        """Instantates a Manifest object.
+
+        An Armada manifest expects that at least one of the following be
+        included in ``documents``:
+
+        * A chart with schema "armada/Chart/v*"
+        * A chartgroup with schema "armada/ChartGroup/v*"
+        * A manifest with schema "armada/Manifest/v*"
+
+        Where "v*" can represent any currently supported version number.
+
+        :param List[dict] documents: Documents out of which to build the
+            Armada manifest.
+        :param str desired_manifest: The desired manifest to run. Useful for
+            specifying which manifest to run when multiple are available.
+            Default is None.
+        """
         self.config = None
         self.documents = documents
-        self.charts = []
-        self.groups = []
-        self.manifest = None
-        self._find_documents()
+        self.charts, self.groups, manifests = self._find_documents(
+            desired_manifest)
 
-    def _find_documents(self):
+        if len(manifests) > 1:
+            error = ('Multiple manifests are not supported. Ensure that the '
+                     '`desired_manifest` option is set to specify the desired '
+                     'manifest.')
+            LOG.error(error)
+            raise exceptions.ManifestException(details=error)
+        else:
+            self.manifest = manifests[0] if manifests else None
+
+        if not all([self.charts, self.groups, self.manifest]):
+            expected_schemas = [
+                const.DOCUMENT_CHART, const.DOCUMENT_GROUP,
+                const.DOCUMENT_MANIFEST
+            ]
+            error = ('Documents must be a list containing a document with one '
+                     'one of the following schemas: %s.')
+            LOG.error(error, expected_schemas)
+            raise exceptions.ManifestException(
+                details=error % expected_schemas)
+
+    def _find_documents(self, desired_manifest=None):
+        charts = []
+        groups = []
+        manifests = []
         for document in self.documents:
-            if document.get('schema') == DOCUMENT_CHART:
-                self.charts.append(document)
-            if document.get('schema') == DOCUMENT_GROUP:
-                self.groups.append(document)
-            if document.get('schema') == DOCUMENT_MANIFEST:
-                self.manifest = document
+            if document.get('schema') == const.DOCUMENT_CHART:
+                charts.append(document)
+            if document.get('schema') == const.DOCUMENT_GROUP:
+                groups.append(document)
+            if document.get('schema') == const.DOCUMENT_MANIFEST:
+                manifest_name = document.get('metadata', {}).get('name')
+                if desired_manifest:
+                    if manifest_name == desired_manifest:
+                        manifests.append(document)
+                else:
+                    manifests.append(document)
+        return charts, groups, manifests
 
     def find_chart_document(self, name):
         for chart in self.charts:
             if chart.get('metadata', {}).get('name') == name:
                 return chart
-        raise ValueError(
-            'Could not find a {} named "{}"'.format(DOCUMENT_CHART, name))
+        raise exceptions.ManifestException(
+            details='Could not find a {} named "{}"'.format(
+                const.DOCUMENT_CHART, name))
 
     def find_chart_group_document(self, name):
         for group in self.groups:
             if group.get('metadata', {}).get('name') == name:
                 return group
-        raise ValueError(
-            'Could not find a {} named "{}"'.format(DOCUMENT_GROUP, name))
+        raise exceptions.ManifestException(
+            details='Could not find a {} named "{}"'.format(
+                const.DOCUMENT_GROUP, name))
 
     def build_charts_deps(self):
         for chart in self.charts:
@@ -67,9 +119,9 @@ class Manifest(object):
                     'chart': chart_dep.get('data')
                 }
         except Exception:
-            raise Exception(
-                "Could not find dependency chart {} in {}".format(
-                    dep, DOCUMENT_CHART))
+            raise exceptions.ManifestException(
+                details="Could not find dependency chart {} in {}".format(
+                    dep, const.DOCUMENT_CHART))
 
     def build_chart_group(self, chart_group):
         try:
@@ -83,9 +135,9 @@ class Manifest(object):
                     'chart': chart_dep.get('data')
                 }
         except Exception:
-            raise Exception(
-                "Could not find chart {} in {}".format(
-                    chart, DOCUMENT_GROUP))
+            raise exceptions.ManifestException(
+                details="Could not find chart {} in {}".format(
+                    chart, const.DOCUMENT_GROUP))
 
     def build_armada_manifest(self):
         try:
@@ -102,9 +154,9 @@ class Manifest(object):
 
                 self.manifest['data']['chart_groups'][iter] = ch_grp_data
         except Exception:
-            raise Exception(
+            raise exceptions.ManifestException(
                 "Could not find chart group {} in {}".format(
-                    group, DOCUMENT_MANIFEST))
+                    group, const.DOCUMENT_MANIFEST))
 
     def get_manifest(self):
         self.build_charts_deps()
