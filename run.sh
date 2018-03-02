0310381984f09991436cc7a4925aebc0bc16e206
One-Fine-Day <vd789v@att.com>
2018-03-02 16:51:09 -0600

    Updated Makefile
    ARMADA-29: CI/CD Armada Testing Framework Entrypoints defined
    
    It does not appear the the docker run command to call tox tests/unit
    is necessary.  The Makefile already includes several testing checks.
    
    Added a few additional targets as specified in the upc-integration
    code-convenstions to satisfy the clcp Makefile standards.
    
    Change-Id: Ia920a856ed6d607fcf38d5f85dbb9a0c8aae4fa6

---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright 2018 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+
+# Create container
+docker run \
+ 		-v $(pwd)/etc:/etc \
+		-d \
+		--name armada-api-test \
+		-p 127.0.0.1:8000:8000 \
+		$IMAGE \
+		server
+
+# Verify container was successfully created
+# If not successfull, print out logs
+GOOD="HTTP/1.1 204 No Content"
+if curl\
+ 		-m 5 \
+		-v \
+		--silent \
+		127.0.0.1:8000/api/v1.0/health \
+		--stderr - \
+		| grep "$GOOD"
+then
+	echo "Health Check Complete";
+else
+	echo "Failed Health Check";
+	docker logs armada-api-test
+fi
+
+# Remove container
+docker rm -fv armada-api-test
